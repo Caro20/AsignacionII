95956eda73f0912680d2769a431982f95fdba207
AriannaAguilarLeiva <arianna.aguilar.leiva@est.una.ac.cr>
2024-03-24 17:36:14 -0600

    Create Header.tsx

---
+++ my-app/src/app/components/Header.tsx
@@ -0,0 +1,27 @@
+// Header.js
+
+import React from 'react';
+import "../styles/Header.css"
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <h1>EasyRecipes</h1>
+            </div>
+            <nav className="navbar">
+                <ul>
+                    <button>
+                        Login
+                    </button>
+                    <button>
+                        Register
+                    </button>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
