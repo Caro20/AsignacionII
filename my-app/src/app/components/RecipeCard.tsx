11ef762c78313fb90e34f67754eaab52dd9c319a
Caro20 <carolina.segura.cordero@est.una.ac.cr>
2024-03-24 21:19:51 -0600

    Ajusten en la implementación de skeletons y suspense.

--- my-app/src/app/components/RecipeCard.tsx
+++ my-app/src/app/components/RecipeCard.tsx
@@ -1,36 +1,64 @@
-import React, { useState, Suspense } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import AddFavoriteRecipe from './AddFavoriteRecipe';
-import "../styles/RecipeCard.css"
-import "../styles/pagination.css"
+import SkeletonComponent from './SkeletonComponent';
+import '../styles/RecipeCard.css';
+import '../styles/pagination.css';
+
 const DisplayComponent: React.FC<{ data: any }> = ({ data }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 4;
   const [showIngredients, setShowIngredients] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [data]);
 
   const toggleIngredients = () => {
     setShowIngredients(!showIngredients);
   };
+
   const renderIngredients = (ingredients: any[]) => {
     return (
-      <ul className='ingredients-list'>
+      <ul className="ingredients-list">
         {ingredients.map((ingredient: any, index: number) => (
           <li key={index}>{ingredient.text}</li>
         ))}
       </ul>
     );
   };
+
   const handleDisplay = () => {
-    if (!data || !data.hits) return;
+    if (loading) {
+      return (
+        <div className="skeleton-container">
+          <SkeletonComponent />
+          <SkeletonComponent />
+          <SkeletonComponent />
+          <SkeletonComponent />
+        </div>
+      );
+    }
+
+    if (!data || !data.hits) return null;
+
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
+
     return data.hits.slice(startIndex, endIndex).map((hit: any) => (
       <div className="recipe-card" key={hit.recipe.uri}>
         <h3 className="recipe-title">{hit.recipe.label}</h3>
         <p className="recipe-cuisineType">{hit.recipe.cuisineType}</p>
         <p className="recipe-dishType">{hit.recipe.dishType}</p>
-        <img className="recipe-image" src={hit.recipe.image} alt={hit.recipe.label} />
+        <img
+          className="recipe-image"
+          src={hit.recipe.image}
+          alt={hit.recipe.label}
+        />
         <AddFavoriteRecipe recipe={hit.recipe} />
-        {/*<button className='show-ingredients' onClick={toggleIngredients}>Show Ingredients</button>*/}
+        <button className="show-ingredients" onClick={toggleIngredients}>
+          Show Ingredients
+        </button>
         {showIngredients && (
           <>
             <h4>Ingredients</h4>
@@ -47,16 +75,27 @@ const DisplayComponent: React.FC<{ data: any }> = ({ data }) => {
 
   return (
     <div className="display-container">
-      <Suspense fallback={<div>Cargando...</div>}>
+      <Suspense
+        fallback={
+          <div className="skeleton-container">
+            <SkeletonComponent />
+            <SkeletonComponent />
+            <SkeletonComponent />
+            <SkeletonComponent />
+          </div>
+        }
+      >
         {handleDisplay()}
       </Suspense>
       {data && data.hits && (
         <div className="pagination">
-          {[...Array(Math.ceil(data.hits.length / itemsPerPage))].map((_, index) => (
-            <button key={index} onClick={() => handlePageChange(index + 1)} className={`pagination-button ${currentPage === index + 1 ? 'active' : ''}`}>
-              {index + 1}
-            </button>
-          ))}
+          {[...Array(Math.ceil(data.hits.length / itemsPerPage))].map(
+            (_, index) => (
+              <button key={index} onClick={() => handlePageChange(index + 1)}>
+                {index + 1}
+              </button>
+            ),
+          )}
         </div>
       )}
     </div>
