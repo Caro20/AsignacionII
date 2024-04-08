b887322d081240ea7dd98ba52435e91a88020a0e
Caro20 <carolina.segura.cordero@est.una.ac.cr>
2024-04-08 11:38:30 -0600

    Reparación del useLocalStorage
    Esto es para que en la página de favoritos se almacene más de una receta y no se borren a través de la navegación Home->Favorites->Home... etc.

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/my-app',
+    
 };
 
 export default nextConfig;
--- my-app/src/app/hooks/useLocalStorage.tsx
+++ my-app/src/app/hooks/useLocalStorage.tsx
@@ -1,4 +1,39 @@
-import { useDebugValue, useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      if (typeof window !== 'undefined') {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      }
+      return initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+export default useLocalStorage;
+
+/*import { useDebugValue, useEffect, useState } from 'react';
 
 export function useLocalStorage<T>(
   key: string,
@@ -27,4 +62,4 @@ function parse<T>(obj: string): T {
   } catch (e) {
     return obj as unknown as T;
   }
-}
+}*/
