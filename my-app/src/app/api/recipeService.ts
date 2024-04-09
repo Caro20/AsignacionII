3b28368fe0a5bf82572486abf23eec2f63e52de1
Caro20 <carolina.segura.cordero@est.una.ac.cr>
2024-04-08 22:24:45 -0600

    Volver a consumir la api.

--- my-app/src/app/api/recipeService.ts
+++ my-app/src/app/api/recipeService.ts
@@ -3,30 +3,30 @@ import axios from 'axios';
 export const fetchData = async (searchTerm: string) => {
   const options = {
     method: 'GET',
-  url: 'https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2',
-  params: {
-    type: 'public',
-    co2EmissionsClass: 'A+',
-    'field[0]': 'uri',
-    beta: 'true',
-    random: 'true',
-    'cuisineType[0]': 'American',
-    'imageSize[0]': 'LARGE',
-    'mealType[0]': 'Breakfast',
-    'health[0]': 'alcohol-cocktail',
-    'diet[0]': 'balanced',
-    'dishType[0]': 'cookies'
-  },
-  headers: {
-    'Accept-Language': 'en',
-    'X-RapidAPI-Key': 'e3de8e6962msh6f52643c5be5b90p16915fjsne69e0d26d9f5',
-    'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
-  }
+    url: 'https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2',
+    params: {
+      type: 'public',
+      co2EmissionsClass: 'A+',
+      'field[0]': 'uri',
+      beta: 'true',
+      random: 'true',
+      'cuisineType[0]': 'American',
+      'imageSize[0]': 'LARGE',
+      'mealType[0]': 'Breakfast',
+      'health[0]': 'alcohol-cocktail',
+      'diet[0]': 'balanced',
+      'dishType[0]': 'Biscuits and cookies',
+    },
+    headers: {
+      'Accept-Language': 'en',
+      'X-RapidAPI-Key': '5133916a96mshafccbc93f47eb20p1f7fc2jsn97483878fbf8',
+      'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com',
+    },
   };
 
   try {
     const response = await axios.request(options);
-    return response.data; // Devuelve los datos de la respuesta
+    return response.data; 
   } catch (error) {
     console.error(error);
     return null;
